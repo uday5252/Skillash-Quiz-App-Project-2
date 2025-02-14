b1426a06ea8451e6110a9c0ad2b9d3be405c6897
N S Pavan nemani <you@example.com>
2025-02-14 14:09:21 +0530

    added gmail integration feature

---
+++ src/GmailIntegration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Timer() {
+    return (
+        <div>
+            Here the code will be written to integrate gmail with quiz app
+        </div>
+    )
+}
+
+export default GmailIntegration
